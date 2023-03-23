690c13497d90c7cda55667950434023126e2b42b
Tasmim Rahman <tasmimrahman2000@gmail.com>
2023-03-23 23:38:25 +0600

    deploy

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(cors());
 app.use(express.json());
 
 //mongodb connection tools
-const uri = process.env.MONGO_DB_URL;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.izqajim.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
